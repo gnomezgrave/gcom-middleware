0f85c70b14b2a47ee4ac8431fa68db5349ee82f5
gnomezgrave <gnomez.grave@gmail.com>
2013-12-15 16:45:28 +0100

    Changes - 2

--- src/gcom/modules/group/ElectionMessage.java
+++ src/gcom/modules/group/ElectionMessage.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package gcom.modules.group;
 
 /**
@@ -10,5 +9,6 @@ package gcom.modules.group;
  * @author Praneeth
  */
 public class ElectionMessage {
-    
+
+    private Member from;
 }
